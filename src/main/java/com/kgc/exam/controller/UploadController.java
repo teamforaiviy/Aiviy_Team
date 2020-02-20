af6993973d3039be12b568d1ea78359f9fefb56b
ChenRuixiang1997 <631671241@qq.com>
2020-02-20 20:55:52 +0800

    修改上传商品Controller

--- src/main/java/com/kgc/exam/controller/UploadController.java
+++ src/main/java/com/kgc/exam/controller/UploadController.java
@@ -3,6 +3,7 @@ package com.kgc.exam.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 /*
 * 文件上传模块
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UploadController {
 
     @RequestMapping("uploadIMG")
-    public void uploadIMG(){
-
+    public void uploadIMG(String gName, String gPrice, String gContent, MultipartFile goodIMG){
+        if(goodIMG.isEmpty()){
+            System.out.println("文件未上传！");
+        }else {
+            String goodIMGName = "";
+        }
     }
 }

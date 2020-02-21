6342a376d9a4f8af1f162badfee67384f3eef93c
ChenRuixiang1997 <631671241@qq.com>
2020-02-21 09:42:43 +0800

    修改上传商品controller

--- src/main/java/com/kgc/exam/controller/UploadController.java
+++ src/main/java/com/kgc/exam/controller/UploadController.java
@@ -1,9 +1,13 @@
 package com.kgc.exam.controller;
 
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /*
 * 文件上传模块
@@ -13,11 +17,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController {
 
     @RequestMapping("uploadIMG")
-    public void uploadIMG(String gName, String gPrice, String gContent, MultipartFile goodIMG){
+    public void uploadIMG(String gName, String gPrice, String gContent, MultipartFile goodIMG) throws IOException {
         if(goodIMG.isEmpty()){
             System.out.println("文件未上传！");
         }else {
-            String goodIMGName = "";
+            String goodIMGName = gName;
+            String ext = FilenameUtils.getExtension(goodIMG.getOriginalFilename());
+            goodIMG.transferTo(new File("D:\\Aiviy_Img\\img\\"+goodIMGName+"."+ext));
         }
     }
 }

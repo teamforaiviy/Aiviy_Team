f26cb06ea2e056741fc1b41ecbf48fe583960271
changjungg <1370544803@qq.com>
2020-02-24 10:08:37 +0800

    Merge branch 'master' of E:\ideal\idea_workspace\Aiviy_Team with conflicts.

--- src/main/java/com/kgc/exam/service/impl/UserServiceImpl.java
+++ src/main/java/com/kgc/exam/service/impl/UserServiceImpl.java
@@ -1,4 +1,25 @@
 package com.kgc.exam.service.impl;
 
-public class UserServiceImpl {
+import com.kgc.exam.entity.User;
+import com.kgc.exam.mapper.UserMapper;
+import com.kgc.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public User login(Integer id) {
+        return userMapper.login(id);
+    }
+
+    @Override
+    public Integer add(User user) {
+        return userMapper.add(user);
+    }
+
 }

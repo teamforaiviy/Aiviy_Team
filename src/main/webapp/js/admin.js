d968b6c1e3b5cc02a9cc6c7290f8c7aef68056d8
msmkdxx <2785413150@qq.com>
2020-02-24 16:24:19 +0800

    添加

--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -1,4 +1,58 @@
+var pn=1;
+var ps=5;
+$(function () {
+    $('#myTabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+    queryUser(pn,ps)
+})
+function queryUser(pn,ps) {
+    $.getJSON("user/queryUser",{"pn":pn,"ps":ps},function (data) {
+        var page =eval(data);
+        var str="";
+        $(page.list).each(function() {
+            str += "<tr>" +
+                "<td>"+this.userId+"</td>" +
+                "<td>"+this.userName+"</td>" +
+                "<td>"+this.nickname+"</td>" +
+                "<td>"+this.userPwd+"</td>" +
+                "<td>"+this.userMail+"</td>" +
+                "<td>"+this.userPhone+"</td>" +
+                "<td>"+(this.userMoney==null?"":this.userMoney)+"</td>" +
+                "<td>"+this.member.vName+"</td>" +
+                "<td><a href='#' data-toggle='modal' data-target='#insertAddress' onclick='addAdd("+this.userId+")'>添加地址</a>&nbsp;&nbsp;<a href='#'>修改</a>&nbsp;&nbsp;<a href='#'>删除</a></td>" +
+                "</tr>";
+        })
+        $("#user #myTable tbody").empty().append(str);
 
+        var pageStr="";
+        pageStr +="<li><a href='javascript:queryUser(1,"+ps+")'>首页</a></li>";
+        if(page.hasPreviousPage){
+            pageStr +="<li>" +
+                "<a href='javascript:queryUser("+(page.pageNum-1)+","+ps+")' aria-label='Previous'>" +
+                "<span aria-hidden=''true'>&laquo;</span>" +
+                "</a>" +
+                "</li>";
+        }
+        $(page.navigatepageNums).each(function () {
+            if(page.pageNum==this){
+                pageStr +="<li><a class='active' href='javascript:queryUser("+this+","+ps+")'>"+this+"</a></li>";
+            }else {
+                pageStr +="<li><a href='javascript:queryUser("+this+","+ps+")'>"+this+"</a></li>";
+            }
+        })
+        if(page.hasNextPage){
+            pageStr +="<li>" +
+                "<a href='javascript:queryUser("+(page.pageNum+1)+","+ps+")' aria-label='Previous'>" +
+                "<span aria-hidden='true'>&raquo;</span>" +
+                "</a>" +
+                "</li>";
+        }
+        pageStr +="<li><a href='javascript:queryUser("+page.pages+","+ps+")'>尾页</a></li>";
+        $("#user .pagination").empty().append(pageStr);
+    })
+}
 /**
  * 添加用户
  */

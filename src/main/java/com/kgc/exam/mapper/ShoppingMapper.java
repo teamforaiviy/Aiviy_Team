3354961ab1d011f300c7d8447d7224b9f0796eed
ChenRuixiang1997 <631671241@qq.com>
2020-03-01 15:54:58 +0800

    更改购物车实体以及mapper、mapper.xml以便于与生成订单对接

--- src/main/java/com/kgc/exam/mapper/ShoppingMapper.java
+++ src/main/java/com/kgc/exam/mapper/ShoppingMapper.java
@@ -13,7 +13,14 @@ public interface ShoppingMapper {
     Integer del(@Param("gId") Integer gId);
     Integer updateuId(@Param("shopping") Shopping shopping);
 
+    /*
+    * 通过用户id以及购买状态删除购物车表中数据
+    * */
     Integer deleteBySStatusAndUserId(@Param("sStatus") Integer sStatus,@Param("userId") Integer userId);
 
+    /*
+    * 更改购物车中数据状态s_status：0->1
+    * 表示以生成订单，为删除购物车中数据做准备
+    * */
     Integer updateByUId(@Param("userId") Integer userId);
 }

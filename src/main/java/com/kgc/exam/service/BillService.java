4c7b9826946668b313d506ea309f5bcf809e5b18
jianzhihao666 <953281860@qq.com>
2020-03-02 10:45:22 +0800

    结算页面获取地址

--- src/main/java/com/kgc/exam/service/BillService.java
+++ src/main/java/com/kgc/exam/service/BillService.java
@@ -3,5 +3,5 @@ package com.kgc.exam.service;
 import com.kgc.exam.entity.Address;
 
 public interface BillService {
-    Integer queryaddress(Integer uId);
+    Address queryaddress(Integer uId);
 }
